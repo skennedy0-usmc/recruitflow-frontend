4c20b7c8bc90686e86ec6fe7aa213a05890721fa
Sean Kennedy <skennedy0@gmail.com>
2025-05-10 18:16:32 -0400

    Fix: Tailwind v4 config adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
